e655ba5dc736332cc44da0616021014cc7efe4d0
Your Nanba <66373962+Rithish-vt@users.noreply.github.com>
2023-10-29 02:19:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Rithish-vt/AutoFilterAdvance /KGAutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO /KGAutofilter
 fi
 cd /KGAutoFilter
 pip3 install -U -r requirements.txt
